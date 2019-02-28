3fdb2b788d2f00641c700f40e662aff6c57ca358
Tim.Cech <Tim.Cech@student.hpi.uni-potsdam.de>
2019-02-28 11:21:24 +0100

    Remove redundant App class

--- src/de/hpi/mod/sim/env/app/App.java
+++
@@ -1,8 +0,0 @@
-package de.hpi.mod.sim.env.app;
-import de.hpi.mod.sim.env.view.DriveSimFrame;
-
-public class App {
-	public static void main(String[] args) {
-		DriveSimFrame.make_window();
-	}
-}
