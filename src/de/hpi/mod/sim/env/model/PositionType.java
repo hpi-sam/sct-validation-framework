a1f5f63a1bf458d63478b74407bdf84d06db74d3
Tim.Cech <Tim.Cech@student.hpi.uni-potsdam.de>
2019-06-10 14:17:00 +0200

    Position type

--- src/de/hpi/mod/sim/env/model/PositionType.java
+++ src/de/hpi/mod/sim/env/model/PositionType.java
@@ -4,5 +4,16 @@ package de.hpi.mod.sim.env.model;
  * The types of cells the robot can stand on
  */
 public enum PositionType {
-    WAYPOINT, STATION, CROSSROAD
+    WAYPOINT, STATION, CROSSROAD;
+    
+    
+    @Override
+    public String toString() {
+      switch(this) {
+        case WAYPOINT: return "Waypoint";
+        case CROSSROAD: return "Crossroad";
+        case STATION: return "Station";
+        default: return "Invalid Position";
+      }
+    }
 }
