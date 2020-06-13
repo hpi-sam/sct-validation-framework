cec4ece1c857bfe68c568d4e7e0af7216d4f2531
simon.wietheger <simon.wietheger@student.hpi.de>
2020-06-13 16:04:23 +0200

    Update reference

--- src/de/hpi/mod/sim/env/model/CellType.java
+++ src/de/hpi/mod/sim/env/model/CellType.java
@@ -2,7 +2,7 @@ package de.hpi.mod.sim.env.model;
 
 /**
  * Similar to {@link PositionType}, but more specific.
- * Is used in the {@link de.hpi.mod.sim.env.setting.infinitestations.GridManagement} and when drawing in the view
+ * Is used in the {@link de.hpi.mod.sim.env.model.IGrid} and when drawing in the view
  */
 public enum CellType {
     WAYPOINT, CROSSROAD, BLOCK, BATTERY, LOADING, STATION, QUEUE;
