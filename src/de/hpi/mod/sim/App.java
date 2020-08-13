9525d5c3b08a8028f870f70b5f94f1a601d99c47
simon.wietheger <simon.wietheger@student.hpi.de>
2020-08-13 19:25:09 +0200

    Fix InfiniteWarehouse selection

--- src/de/hpi/mod/sim/App.java
+++ src/de/hpi/mod/sim/App.java
@@ -17,7 +17,7 @@ import de.hpi.mod.sim.core.view.SimulatorFrame;
 public class App {
 
     private static final String[] POSSIBLE_WORLDS = {
-            "de.hpi.mod.sim.world.infinitewarehouse.InfiniteWarehouse"
+            "de.hpi.mod.sim.worlds.infinitewarehouse.InfiniteWarehouse"
      };
 
      private static World selectWorld() {
